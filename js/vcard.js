018dc418dcf4f7a5bff86bd065579bc8d6e24584
Kim Tholstorf <kim@tholstorf.dk>
2021-03-19 21:44:05 +0100

    Create vcard.js

---
+++ js/vcard.js
@@ -0,0 +1,29 @@
+<script>
+			var count = 2; // Number of remaining seconds.
+			var counter; // Handle for the countdown event.
+			
+			function start() {
+				counter = setInterval(timer, 1000);
+			}
+
+			function timer() {
+				// Show the number of remaining seconds on the web page.
+				var output = document.getElementById("displaySeconds");
+				//output.innerHTML = count;
+				
+				// Decrease the remaining number of seconds by one.
+				count--;
+				
+				// Check if the counter has reached zero.
+				if (count < 0) { // If the counter has reached zero...
+					// Stop the counter.
+					clearInterval(counter);
+					
+					// Start the download.
+					window.location.href = "https://kim.tholstorf.dk/personal_vcard.vcf";
+					return;
+				}
+			}		
+			// Start the countdown timer when the page loads. 
+			window.addEventListener("load", start, false);
+		</script>
