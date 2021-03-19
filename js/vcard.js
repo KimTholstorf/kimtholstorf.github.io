755abb77dffe49734cf7561ee92726e9c38fb81e
Kim Tholstorf <kim@tholstorf.dk>
2021-03-19 21:58:10 +0100

    Update vcard.js

--- js/vcard.js
+++ js/vcard.js
@@ -1,29 +1,29 @@
 <script>
-			var count = 2; // Number of remaining seconds.
-			var counter; // Handle for the countdown event.
+	var count = 1; // Number of remaining seconds.
+	var counter; // Handle for the countdown event.
 			
-			function start() {
-				counter = setInterval(timer, 1000);
-			}
+	function start() {
+		counter = setInterval(timer, 1000);
+	}
 
-			function timer() {
-				// Show the number of remaining seconds on the web page.
-				var output = document.getElementById("displaySeconds");
-				//output.innerHTML = count;
+	function timer() {
+		// Show the number of remaining seconds on the web page.
+		var output = document.getElementById("displaySeconds");
+		//output.innerHTML = count;
 				
-				// Decrease the remaining number of seconds by one.
-				count--;
+		// Decrease the remaining number of seconds by one.
+		count--;
 				
-				// Check if the counter has reached zero.
-				if (count < 0) { // If the counter has reached zero...
-					// Stop the counter.
-					clearInterval(counter);
-					
-					// Start the download.
-					window.location.href = "https://kim.tholstorf.dk/personal_vcard.vcf";
-					return;
-				}
-			}		
-			// Start the countdown timer when the page loads. 
-			window.addEventListener("load", start, false);
-		</script>
+		// Check if the counter has reached zero.
+		if (count < 0) { // If the counter has reached zero...
+			// Stop the counter.
+			clearInterval(counter);
+
+			// Start the download.
+			window.location.href = "https://kim.tholstorf.dk/personal_vcard.vcf";
+			return;
+		}
+	}		
+	// Start the countdown timer when the page loads. 
+	window.addEventListener("load", start, false);
+</script>
